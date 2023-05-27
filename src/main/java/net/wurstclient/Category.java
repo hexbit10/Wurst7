ecd975462a10cabfdc1e11b3efff225e07d1227e
hexax <techhelp1060@gmail.com>
2023-05-27 20:13:09 +0200

    🎨 Add Colors to Categories

--- src/main/java/net/wurstclient/Category.java
+++ src/main/java/net/wurstclient/Category.java
@@ -7,26 +7,31 @@
  */
 package net.wurstclient;
 
+import java.awt.Color;
+
 public enum Category
 {
-	BLOCKS("Blocks"),
-	MOVEMENT("Movement"),
-	COMBAT("Combat"),
-	RENDER("Render"),
-	CHAT("Chat"),
-	FUN("Fun"),
-	ITEMS("Items"),
-	OTHER("Other");
-	
+	RENDER("Render", new Color(0x37FF00)),
+	COMBAT("Combat", new Color(0xFF1313)),
+	MOVEMENT("Movement", new Color(0x0059FF)),
+	BLOCKS("Blocks", new Color(0xFF7600)),
+	CHAT("Chat", new Color(0xFFF20B)),
+	ITEMS("Items", new Color(0x00FF9C)),
+	FUN("Fun", new Color(0xAFFF0A)),
+	OTHER("Other", new Color(0xFF0DFF));
+
 	private final String name;
-	
-	private Category(String name)
+	private final Color color;
+
+	private Category(String name, Color color)
 	{
 		this.name = name;
+		this.color = color;
 	}
-	
+
 	public String getName()
 	{
 		return name;
 	}
+	public Color getColor() { return color; }
 }
